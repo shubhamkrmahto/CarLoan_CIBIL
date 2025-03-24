66b34eb750b33b09e63ce3e1849383f2e1fc6c4b
sushantkhilare25 <sskhilare25@gmail.com>
2025-03-24 15:01:13 +0530

    GET API Created by Sushant.

--- CLMS-CIBIL-API-Module/src/main/java/com/app/controller/CibilController.java
+++ CLMS-CIBIL-API-Module/src/main/java/com/app/controller/CibilController.java
@@ -3,6 +3,8 @@ package com.app.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,5 +30,22 @@ public class CibilController {
 		return new ResponseEntity<String>("Your Cibil Data has been Registered Successfully.", HttpStatus.CREATED);  
 	}
 	
+	
+
+	@GetMapping("/getCibilAllData")
+	public ResponseEntity<Iterable<CibilEntity>> getAllData()
+	{
+		Iterable<CibilEntity> allData =service.findAll();
+		
+		return new ResponseEntity<Iterable<CibilEntity>>(allData,HttpStatus.OK);
+	}
+	
+	@GetMapping("/getCibilSingleData/{cibilId}")
+	public ResponseEntity<CibilEntity> getSingleData(@PathVariable("cibilId")Integer id)
+	{
+		CibilEntity cibil=service.getSingleData(id);
+		
+		return new ResponseEntity<CibilEntity>(cibil, HttpStatus.OK);
+	}
 
 }

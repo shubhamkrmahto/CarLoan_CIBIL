6a7995c77abbe9fd79d298140e377136fde7a594
sushantkhilare25 <sskhilare25@gmail.com>
2025-03-24 14:44:23 +0530

    GET API Created by Sushant

--- CLMS-CIBIL-API-Module/src/main/java/com/app/service/CibilServiceI.java
+++ CLMS-CIBIL-API-Module/src/main/java/com/app/service/CibilServiceI.java
@@ -1,5 +1,11 @@
 package com.app.service;
 
+import com.app.entity.CibilEntity;
+
 public interface CibilServiceI {
+
+	public Iterable<CibilEntity> findAll();
+
+	public CibilEntity getSingleData(Integer id);
 
 }
--- CLMS-CIBIL-API-Module/src/main/java/com/app/serviceimpl/CibilServiceImpl.java
+++ CLMS-CIBIL-API-Module/src/main/java/com/app/serviceimpl/CibilServiceImpl.java
@@ -1,8 +1,11 @@
 package com.app.serviceimpl;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.app.entity.CibilEntity;
 import com.app.repo.CibilRepository;
 import com.app.service.CibilServiceI;
 @Service
@@ -10,4 +13,19 @@ public class CibilServiceImpl implements CibilServiceI {
 	@Autowired
 	CibilRepository cibilRepo;
 
+	@Override
+	public Iterable<CibilEntity> findAll() {
+		
+		
+		return cibilRepo.findAll();
+	}
+
+	@Override
+	public CibilEntity getSingleData(Integer id) {
+		Optional<CibilEntity> cibilById = cibilRepo.findById(id);
+		
+		return cibilById.get();
+	}
+
+	
 }

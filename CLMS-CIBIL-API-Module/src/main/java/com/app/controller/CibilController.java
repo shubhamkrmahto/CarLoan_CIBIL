8b4ff5219a55f71b4013e9d8c12980e453a7f3b9
Priya-M11 <priyamhanare@gmail.com>
2025-03-25 10:58:22 +0530

    DELETE API By ID added in Cibil Module by Priya M.

--- CLMS-CIBIL-API-Module/src/main/java/com/app/controller/CibilController.java
+++ CLMS-CIBIL-API-Module/src/main/java/com/app/controller/CibilController.java
@@ -3,6 +3,8 @@ package com.app.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -36,5 +38,12 @@ public class CibilController {
 		   
 	   }
 	
+		@DeleteMapping("/deleteCibilEnquiry/{id}")
+	 public ResponseEntity<String> deleteCibilData(@PathVariable ("id") Integer id)
+	 {
+			service.deleteCibilEnquiry(id);
+			return new ResponseEntity<String>("Delete Your cibil Field Successfully...!",HttpStatus.OK);
+
+	 }
 
 }

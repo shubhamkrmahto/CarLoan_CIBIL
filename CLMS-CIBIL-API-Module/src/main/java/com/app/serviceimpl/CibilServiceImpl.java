ca69184b7ed022d6b7ac1a737064e90de3bec451
Suraj <ingolesuraj53@gmail.com>
2025-04-01 10:45:10 +0530

    final push Merge branch 'master' of https://github.com/shubhamkrmahto/CarLoan_CIBIL into suraj

--- CLMS-CIBIL-API-Module/src/main/java/com/app/serviceimpl/CibilServiceImpl.java
+++ CLMS-CIBIL-API-Module/src/main/java/com/app/serviceimpl/CibilServiceImpl.java
@@ -6,13 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.app.entity.CibilEntity;
-
 import com.app.enums.CibilStatusEnum;
 import com.app.repo.CibilRepository;
 import com.app.service.CibilServiceI;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 @Service
 public class CibilServiceImpl implements CibilServiceI {
 	@Autowired
@@ -26,15 +22,15 @@ public class CibilServiceImpl implements CibilServiceI {
 		if (byId.isPresent()) {
 			CibilEntity entity = byId.get();
 			entity.setCibilScore(cibil.getCibilScore());
-			entity.setCibilScoreDateTime(cibil.getCibilScoreDateTime());
+			entity.setCibilScoreDateTime(LocalDateTime.now());
 			entity.setStatus(cibil.getStatus());
 			entity.setCibilRemark(cibil.getCibilRemark());
 			
 			cibilRepo.save(entity);
-			
+			  
 			return "Data Updated Successfully";
 		}
-		return null;	
+		return "Record not found";	
 	}
 	public CibilEntity save(CibilEntity ce) {
 		

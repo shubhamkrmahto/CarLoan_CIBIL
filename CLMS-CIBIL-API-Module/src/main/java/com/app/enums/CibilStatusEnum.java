ab3dbfbb74a1231d6266c02b4e2b80ce534931ac
shubhamkrmahto <skm9258@gmail.com>
2025-05-18 18:18:38 +0530

    Repository and Entity deleted

--- CLMS-CIBIL-API-Module/src/main/java/com/app/enums/CibilStatusEnum.java
+++
@@ -1,7 +0,0 @@
-package com.app.enums;
-
-public enum CibilStatusEnum {
-	
-	APPROVED,PENDING,REJECTED;
-
-}
--- CLMS-CIBIL-API-Module/src/main/java/com/app/serviceimpl/CibilServiceImpl.java
+++ CLMS-CIBIL-API-Module/src/main/java/com/app/serviceimpl/CibilServiceImpl.java
@@ -3,7 +3,6 @@ package com.app.serviceimpl;
 import java.util.Random;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.app.repo.CibilRepository;
 import com.app.service.CibilServiceI;
 
 @Service
@@ -11,9 +10,6 @@ public class CibilServiceImpl implements CibilServiceI {
 
 	private final Random random = new Random();
 	
-	@Autowired
-	CibilRepository cibilRepo;
-
 
 	@Override
 	public Integer generateCibil() {
